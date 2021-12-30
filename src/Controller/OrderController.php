8698eecc9c33480f2c98c49fa5a6cceaa5a341bd
chebl <chebl.mahmoud@gmail.com>
2021-12-30 19:10:33 +0100

    Ajout de la livraison dans les informations envoyées à Stripe

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -70,6 +70,8 @@ class OrderController extends AbstractController
 
             //Enregister ma commande Order()
             $order = new Order();
+            $reference = date('Ymd') . '-' . uniqid();
+            $order->setReference($reference);
             $order->setUser($this->getUser());
             $order->setCreatedAt($date);
             $order->setCarrierName($carriers->getName());
@@ -93,7 +95,8 @@ class OrderController extends AbstractController
 
                 'cart' => $cart->getFull(),
                 'carrier' => $carriers,
-                'delivery' => $delivery_content
+                'delivery' => $delivery_content,
+                'reference' => $order->getReference(),
             ]);
         }
         return $this->redirectToRoute('cart');
--- src/Controller/StripeController.php
+++ src/Controller/StripeController.php
@@ -4,37 +4,60 @@ namespace App\Controller;
 
 use Stripe\Stripe;
 use App\Classe\Cart;
-use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Order;
+use App\Entity\Product;
 use Stripe\Checkout\Session;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class StripeController extends AbstractController
 {
     /**
-     * @Route("/commande/create-session", name="stripe_create_session")
+     * @Route("/commande/create-session/{reference}", name="stripe_create_session")
      */
-    public function index(Cart $cart)
+    public function index(EntityManagerInterface $entityManager, Cart $cart, $reference)
     {
         $products_for_stripe = [];
         $YOUR_DOMAIN = 'http://127.0.0.1:8000';
+        $order = $entityManager->getRepository(Order::class)->findOneByReference($reference);
+        if (!$order) {
+            new JsonResponse(['error' => 'order']);
+        }
+        foreach ($order->getOrderDetails()->getValues()  as $product) {
+            $product_object = $entityManager->getRepository(Product::class)->findOneByName($product->getProduct());
 
-        foreach ($cart->getFull() as $product) {
             $products_for_stripe[] = [
                 'price_data' => [
                     'currency' => 'eur',
-                    'unit_amount' => $product['product']->getPrice(),
+                    'unit_amount' =>  $product->getPrice(),
                     'product_data' => [
-                        'name' => $product['product']->getName(),
-                        'images' => [$YOUR_DOMAIN . "/uploads/" . $product['product']->getIllustration()],
+                        'name' => $product->getProduct(),
+                        'images' => [$YOUR_DOMAIN . "/uploads/" . $product_object->getIllustration()],
                     ],
                 ],
-                'quantity' => $product['quantity'],
+                'quantity' => $product->getQuantity(),
             ];
         }
+        $products_for_stripe[] = [
+            'price_data' => [
+                'currency' => 'eur',
+                'unit_amount' => $order->getCarrierPrice() * 100,
+                'product_data' => [
+                    'name' => $order->getCarrierName(),
+                    'images' => [$YOUR_DOMAIN],
+                ],
+            ],
+            'quantity' => 1,
+
+        ];
 
         Stripe::setApiKey('sk_test_51KCQwzGL2Kvpsr1r07VcHuT5KtQTpEK1XiyDh8q6BqRIH01INuQgJ6MiZoYBsQPJao13kvcJrmORQZgAVx1nZPKc00HGF5Vdd9');
 
         $checkout_session = Session::create([
+
+            'customer_email' => $this->getUser()->getEmail(),
             'line_items' => [
                 $products_for_stripe
             ],
